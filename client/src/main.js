f7e58e752ebb5c51c30d4f065ff878095f2ec647
mocmeo <vdev.lamnn@gmail.com>
2019-06-25 11:11:08 +0700

    Experiment with ant-design components to create header

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import Antd from "ant-design-vue";
+import router from "./router";
+import store from "./store";
+import "ant-design-vue/dist/antd.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: h => h(App)
+}).$mount("#app");
