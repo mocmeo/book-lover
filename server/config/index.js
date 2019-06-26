01fe9b5fb8557dd4f376a7f106f018a96116833a
mocmeo <vdev.lamnn@gmail.com>
2019-06-26 10:22:57 +0700

    Config mongodb server on digitalocean to allow connect remotely

--- server/config/index.js
+++ server/config/index.js
@@ -2,5 +2,5 @@ module.exports = {
 	PORT: process.env.port || 4000,
 	MONGO_URL:
 		process.env.MONGO_URL ||
-		"mongodb://mocmeo:test123@ds241647.mlab.com:41647/meowtain-book"
+		"mongodb://mocmeo:test123@159.65.14.173:27017/meowtain-book"
 };
