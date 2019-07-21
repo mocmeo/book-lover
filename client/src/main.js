db88ed85f290b6ad26d173c028b21946af470d6f
mocmeo <vdev.lamnn@gmail.com>
2019-07-21 21:04:07 +0700

    Add vue bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,15 @@ import Antd from "ant-design-vue";
 import router from "./router";
 import store from "./store";
 import "ant-design-vue/dist/antd.css";
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
+Vue.use(BootstrapVue);
 
 new Vue({
 	router,
