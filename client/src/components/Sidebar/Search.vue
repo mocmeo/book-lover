421c4e1ce5fcfea6db9edd18d56d8c26a933e046
mocmeo <vdev.lamnn@gmail.com>
2019-07-23 09:54:38 +0700

    Refactor to VueJS components

---
+++ client/src/components/Sidebar/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Search -->
+  <section id="search" class="alt">
+    <form method="post" action="#">
+      <input type="text" name="query" id="query" placeholder="Search" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search"
+};
+</script>
+
+<style>
+</style>
---
+++ client/src/components/Sidebar/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="inner">
+    <!-- Search -->
+    <search />
+
+    <!-- Menu -->
+    <book-menu />
+
+    <!-- Top Books -->
+    <top-books />
+
+    <!-- Contact  -->
+    <contact />
+
+    <!-- Footer -->
+    <footer id="footer">
+      <p class="copyright">
+        &copy; Untitled. All rights reserved. Demo Images:
+        <a href="https://unsplash.com">Unsplash</a>. Design:
+        <a href="https://html5up.net">HTML5 UP</a>.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import BookMenu from "./Menu";
+import Search from "./Search";
+import Contact from "./Contact";
+import TopBooks from "./TopBooks";
+export default {
+  name: "Sidebar",
+  components: {
+    BookMenu,
+    Search,
+    Contact,
+    TopBooks
+  }
+};
+</script>
+
+<style>
+</style>
