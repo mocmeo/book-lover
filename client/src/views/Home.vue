c2e385fe3d2eb16fc286f6cb64f2dd379571aacf
mocmeo <vdev.lamnn@gmail.com>
2019-07-23 14:20:29 +0700

    Remove unused assets,css & use cdn instead

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="cover bg-primary section-bg">
     <page-header />
-    <intro />
   </div>
 </template>
 
 <script>
-import Intro from "@/components/Intro.vue";
 import PageHeader from "@/components/Header.vue";
 export default {
   name: "home",
   components: {
-    Intro,
     PageHeader
   }
 };
